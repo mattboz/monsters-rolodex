693eaf36a3d85297c316ab744a2870095baf8307
Matthew Micallef <matthew.micallef@mrgreen.com>
2020-03-04 16:48:13 +0100

    Section 3 - 26

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [] 
+    };
+  }
+
+  componentDidMount(){ //this is run when react is pushed into the dom for the first time
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. And you Golden woop woop mother fuckers
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map(monsters => (
+            <h1 key={monsters.id}>{monsters.name}</h1>
+          ))}
       </div>
     );
   }
